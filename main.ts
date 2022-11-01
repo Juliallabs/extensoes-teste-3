1d4e3e7b283dbe6d21a9582675dd24c37e53fa84
Julialabs <72475374+Juliallabs@users.noreply.github.com>
2022-11-01 20:23:02 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -18,13 +18,26 @@ namespace EletroBlocks {
     //% block
     export function leituraAnalogica(porta: AnalogPin, sensor: TIPOS): number {
         let x = pins.analogReadPin(porta)
+        
         if (pins.analogReadPin(porta) > 980) {
+
             x = 1023
         }
         if (pins.analogReadPin(porta) < 50) {
-            x = 0
-        }
 
+            x = 0
+
+        } else {
+
+            let mediamovel = 0
+
+            for (let i = 0 ;i <11; i++) {
+            
+                mediamovel = mediamovel + pins.analogReadPin(porta)
+
+            }
+                x = mediamovel /10
+        }
         return x
     }
 }
