5df9f811860f419da168ea5f37267747f1354cfa
CarvalhoCortes <fernando.cortes@ewally.com.br>
2022-11-01 22:12:54 -0300

    CodeRefactor

--- main.ts
+++ main.ts
@@ -1,52 +1,40 @@
-
 /**
-* Functions are mapped to blocks using various macros
-* in comments starting with %. The most important macro
-* is "block", and it specifies that a block should be
-* generated for an **exported** function.
-*/
-enum TIPOS {
-    //% block="potenciometro"
-    MicroSeconds,
-    //% block="ldr"
-    Centimeters,
-    //% block="infra vermelho"
-    Inches
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+enum Types {
+  //% block="potentiometer"
+  MicroSeconds,
+  //% block="ldr"
+  CentiMeters,
+  //% block="infra red"
+  Inches,
 }
 //% color="#AA278D" weight=100
 namespace EletroBlocks {
-    //% block
-    export function leituraAnalogica(porta: AnalogPin, sensor: TIPOS): number {
-        let x = 0
-        let mediamovel1 = 0
-        let mediamovel2 = 0
+  //% block
+  export function analogReads(port: AnalogPin): Number {
+    let sum1 = 0
+    let sum2 = 0
+    let topLimit = 930
+    let bottomLimit = 50
+    let samples = 20
 
-        for (let j = 0; j < 19; j++) {
-
-            for (let i = 0 ;i <9; i++) {
-            
-                mediamovel1 = mediamovel1 + pins.analogReadPin(porta)
-
-            }
-
-            mediamovel1 = mediamovel1 /10
-            mediamovel2 = mediamovel2+mediamovel1
-
-            }
-        x = mediamovel2 / 20
-
-
-        if (x > 930) {
-
-            x = 1023
-        }
-
-        if (x < 50) {
-
-            x = 0
-
-        }
-        
-        return Math.round(x)
+    for (let sample2 = 1; sample2 <= samples; sample2++) {
+      for (let sample1 = 1; sample1 <= samples / 2; sample1++) {
+        sum1 += pins.analogReadPin(port)
+      }
+      let arithmeticMovingAverage1 = sum1 / (samples / 2)
+      sum2 += arithmeticMovingAverage1
     }
+
+    let arithmeticMovingAverage = sum2 / samples
+
+    if (arithmeticMovingAverage > topLimit) arithmeticMovingAverage = 1023
+    if (arithmeticMovingAverage < bottomLimit) arithmeticMovingAverage = 0
+
+    return Math.round(arithmeticMovingAverage)
+  }
 }
