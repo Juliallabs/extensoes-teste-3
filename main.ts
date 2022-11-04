8f1c807369d6b96591601d8f6e20a056eddd3885
Julialabs <72475374+Juliallabs@users.noreply.github.com>
2022-11-03 21:51:51 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -13,7 +13,6 @@ enum TIPOS {
     //% block="INFRA VERMELHO"
     Reflexao
 }
-
 enum PORTASIN {
     //% block="ENTRADA1"
     E1,
@@ -22,7 +21,6 @@ enum PORTASIN {
     //% block="ENTRADA3"
     E3
 }
-
 enum PORTASSA {
     //% block="SAIDA_A"
     SA,
@@ -39,44 +37,42 @@ namespace EletroBlocks {
         let x = 0
         let mediamovel1 = 0
         let mediamovel2 = 0
-
         for (let j = 0; j < 19; j++) {
+            for (let i = 0; i < 19; i++) {
 
-            for (let i = 0 ;i <19; i++) {
-            
                 mediamovel1 = mediamovel1 + pins.analogReadPin(porta)
-
             }
-
-            mediamovel1 = mediamovel1 /20
-            mediamovel2 = mediamovel2+mediamovel1
-
-            }
+            mediamovel1 = mediamovel1 / 20
+            mediamovel2 = mediamovel2 + mediamovel1
+        }
         x = mediamovel2 / 20
-
-
         if (x > 930) {
-
             x = 1023
         }
-
         if (x < 50) {
-
             x = 0
-
         }
-  
+
         return Math.round(x)
     }
+    //% block
+    export function leituraTESTE(porta: DigitalPin, sensor: TIPOS):  number{ 
+        let duty
+            pins.onPulsed(porta, PulseValue.High, () => {
+                duty = pins.pulseDuration() / 1000 * 1023
+            })
+        return duty
+       
+    }
+
+
     //% block
     export function leituraDigital(porta_entrada: PORTASIN, sensor: TIPOS): number {
         let porta = port_selec_en(porta_entrada)
         let x = leituraAnalogica(porta, sensor)
-
         if (x > 1024 / 2) {
             return 1
-        }else return 0
-
+        } else return 0
     }
     //% block
     export function saida(porta_saida: PORTASSA): AnalogPin {
@@ -85,9 +81,7 @@ namespace EletroBlocks {
             case PORTASSA.SB: return AnalogPin.P4;
             case PORTASSA.SC: return AnalogPin.P5;
         }
-
     }
-
     // funcoes 
     export function port_selec_en(porta_entrada: PORTASIN): AnalogPin {
         switch (porta_entrada) {
